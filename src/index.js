d2584b1a7ee21bb4495496571a13ab187ac68721
lena-r-a <rizhankova.elena@gmail.com>
2021-03-10 12:06:49 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -2,5 +2,13 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix){return []};
+  var length = matrix.length;
+  var matrix_r=[];
+  for (i=0;i<length;i++) {
+    if (i%2==1){matrix_r.push(matrix[i].reverse())}
+    else {matrix_r.push(matrix[i])}
+  }
+  var merged = [].concat.apply([],matrix_r);
+  return merged;
 }
